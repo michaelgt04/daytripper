608c35049a1066df8beaca022fbd410fbe89cb3c
michaelgt04 <michaelgt04@gmail.com>
2016-10-10 11:36:32 -0400

    Fixed non-functioning select field

--- react/src/components/Form.js
+++ react/src/components/Form.js
@@ -14,7 +14,7 @@ const Form = props => {
             <input type="text" name="range" placeholder="Enter range (in miles) here..." onChange={props.handleRangeChange}/>
           </div>
           <div className="input-field col s3">
-            <select name="type" onChange={props.handleTypeChange}>
+            <select name="type" className="browser-default" onChange={props.handleTypeChange}>
               <option value="food">Food</option>
               <option value="learn">Learn</option>
               <option value="nature">Nature</option>
